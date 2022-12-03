0c76c5cfbf71cfddd33a19e966ecd17b32c39574
ShounakDeshmukh <shounsach@gmail.com>
2022-12-03 17:24:27 +0530

    Scrapped mnist builder

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-echo Hello World  
+mkdir -p mnist
+curl http://yann.lecun.com/exdb/mnist/train-images-idx3-ubyte.gz --output "mnist/train-images-idx3-ubyte.gz"
+curl http://yann.lecun.com/exdb/mnist/train-labels-idx1-ubyte.gz --output "mnist/train-labels-idx1-ubyte.gz"
+curl http://yann.lecun.com/exdb/mnist/t10k-images-idx3-ubyte.gz --output "mnist/t10k-images-idx3-ubyte.gz"
+curl http://yann.lecun.com/exdb/mnist/t10k-labels-idx1-ubyte.gz --output "mnist/t10k-labels-idx1-ubyte.gz"
+gzip -d /tmp/rust-autograd/data/mnist/*.gz
